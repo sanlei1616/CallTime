1efdac4532f4dd815226c49aaff9021ad0b42b0e
sanlei <1616sanlei@163.com>
2014-04-29 17:42:59 +0800

    some modification

---
+++ src/com/example/calltime/CallTimeUnitTest.java
@@ -0,0 +1,14 @@
+package com.example.calltime;
+
+import junit.framework.TestCase;
+
+public class CallTimeUnitTest extends TestCase {
+
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+	}
+
+	
+}
